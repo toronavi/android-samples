f518a8fde2ea7be0b672e551e3d71745ad0a08aa
toronavi <toronavicom@gmail.com>
2020-01-28 22:33:45 +0900

    Add data source

---
+++ DataBindingSample/app/src/main/java/com/tf/android/databindingsample/model/model/User.kt
@@ -0,0 +1,41 @@
+package com.tf.android.databindingsample.model.model
+
+data class User(
+    val id: String,
+    val name: String,
+    val iconId: Int,
+    val isFavorite: Boolean
+)
+
+class UserBuilder {
+
+    private var mId: String = ""
+    private var mName: String = ""
+    private var mIconId: Int = 0
+    private var mIsFavorite: Boolean = false
+
+    fun setId(id: String): UserBuilder {
+        mId = id
+        return this
+    }
+
+    fun setName(name: String): UserBuilder {
+        mName = name
+        return this
+    }
+
+    fun setIconId(iconId: Int): UserBuilder {
+        mIconId = iconId
+        return this
+    }
+
+    fun setIsFavorite(isFavorite: Boolean): UserBuilder {
+        mIsFavorite = isFavorite
+        return this
+    }
+
+    fun build(): User {
+        return User(mId, mName, mIconId, mIsFavorite)
+    }
+
+}
