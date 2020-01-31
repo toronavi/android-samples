aa25501bb8217bee42ef32cf881e012b57078b51
toronavi <toronavicom@gmail.com>
2020-01-31 14:45:54 +0900

    Add screen

---
+++ DataBindingSample/app/src/main/java/com/tf/android/databindingsample/view/adapter/CommonBindingAdapter.kt
@@ -0,0 +1,32 @@
+package com.tf.android.databindingsample.view.adapter
+
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import com.tf.android.databindingsample.R
+
+object CommonBindingAdapter {
+
+    @JvmStatic
+    @BindingAdapter("app:goneUnless")
+    fun goneUnless(view: View, visible: Boolean) {
+        view.visibility = if (visible) View.VISIBLE else View.GONE
+    }
+
+    @JvmStatic
+    @BindingAdapter("app:iconId")
+    fun setIconById(view: AppCompatImageView, iconId: Int) {
+        val resourceId = when (iconId) {
+            1 -> R.drawable.avatar_01
+            2 -> R.drawable.avatar_02
+            3 -> R.drawable.avatar_03
+            4 -> R.drawable.avatar_04
+            else -> null
+        }
+
+        resourceId?.let {
+            view.setImageResource(it)
+        } ?: view.setImageDrawable(null)
+    }
+
+}
